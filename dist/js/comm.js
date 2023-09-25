d1ae9df3502cecb7b4d6abea4c594990d2487e99
z1xun <jiseonn97@naver.com>
2023-09-25 17:49:59 +0900

    home 수정

--- dist/js/comm.js
+++ dist/js/comm.js
@@ -3,5 +3,29 @@ $(document).ready(function () {
         autoScrolling: true,
         scrollHorizontally: true,
         navigation: true,
+        anchors: ["firstPage", "secondPage", "thirdPage", "fourthPage", "fifthPage"],
+        afterLoad: function (anchorLink, index) {
+            if (index == 1) {
+                $(".about-img, .info-title, .info-cont, .slide-item").removeClass("show");
+                $(".home-wrap").css({
+                    width: "450px",
+                    height: "450px",
+                });
+                $(".home-wrap").animate(
+                    {
+                        opacity: 1,
+                    },
+                    100
+                );
+            }
+        },
+        onLeave: function (index, nextIndex, direction) {
+            if (index == 1 && direction == "down") {
+                $(".home-wrap").css({
+                    width: "4000px",
+                    height: "2000px",
+                });
+            }
+        },
     });
 });
